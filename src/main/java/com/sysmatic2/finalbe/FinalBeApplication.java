09211b92f5e24d882d06cf027afb6fe64d40e2e9
kdh10806 <kimdaehyun10806@gmail.com>
2024-11-04 14:59:50 +0900

    Initialized

---
+++ src/main/java/com/sysmatic2/finalbe/FinalBeApplication.java
@@ -0,0 +1,13 @@
+package com.sysmatic2.finalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysmatic2/finalbe/FinalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmatic2.finalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
