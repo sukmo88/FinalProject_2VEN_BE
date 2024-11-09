323c373a33aacdae8dc51022ff723d96ffd0c420
haechangchoi <buraldongja@naver.com>
2024-11-09 21:17:20 +0900

    feat: [공지사항&FAQ&상담] 상담&공지사항 엔티티
    상담&공지사항 엔티티
    
    -첨부파일 ERD 완료 후 첨부파일 연관관계 설정해야 함

--- src/main/java/com/sysmatic2/finalbe/cs/entity/Consultation.java
+++ src/main/java/com/sysmatic2/finalbe/cs/entity/Consultation.java
@@ -1,52 +1,58 @@
-package com.sysmatic2.finalbe.cs.entity;
+  package com.sysmatic2.finalbe.cs.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+  import jakarta.persistence.Column;
+  import jakarta.persistence.Entity;
+  import jakarta.persistence.GeneratedValue;
+  import jakarta.persistence.GenerationType;
+  import jakarta.persistence.Id;
+  import jakarta.persistence.Table;
+  import java.time.LocalDateTime;
+  import lombok.AllArgsConstructor;
+  import lombok.Builder;
+  import lombok.Data;
+  import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "consultation")
-public class Consultation {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "consultation_id")
-  private Long id;
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  @Entity
+  @Table(name = "consultation")
+  public class Consultation {
 
-  @Column(name = "sender_id", nullable = false)
-  private Long senderId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "consultation_id")
+    private Long id;
 
-  @Column(name = "receiver_id", nullable = false)
-  private Long receiverId;
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;
 
-  @Column(name = "title", nullable = false, length = 255)
-  private String title;
+    @Column(name = "receiver_id", nullable = false)
+    private Long receiverId;
 
-  @Column(name = "content", nullable = false, columnDefinition = "TEXT")
-  private String content;
+    @Column(name = "strategy_id", nullable = false)
+    private Long strategyId;
 
-  @Column(name = "sent_at", nullable = false)
-  private LocalDateTime sentAt;
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
 
-  @Column(name = "updated_at")
-  private LocalDateTime updatedAt;
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
 
-  @Column(name = "is_read")
-  private Boolean isRead;
+    @Column(name = "sent_at", nullable = false)
+    private LocalDateTime sentAt;
 
-  @Column(name = "is_answered", nullable = false)
-  private Boolean isAnswered;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-  @Column(name = "answered_at")
-  private LocalDateTime answeredAt;
-}
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead;
+
+    @Column(name = "is_answered", nullable = false)
+    private Boolean isAnswered ;
+
+    @Column(name = "answered_at")
+    private LocalDateTime answeredAt;
+  }
