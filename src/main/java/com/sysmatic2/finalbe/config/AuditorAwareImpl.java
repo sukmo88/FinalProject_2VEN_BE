864b31c0f1b71b5a6a5ba141e6afe9ad262c6f4a
Jamieie <116743317+Jamieie@users.noreply.github.com>
2024-11-09 21:32:29 +0900

    Feat: SpringSecurity 추가 및 AuditorAwareImpl 에서 memberId 반환하도록 수정

--- src/main/java/com/sysmatic2/finalbe/config/AuditorAwareImpl.java
+++ src/main/java/com/sysmatic2/finalbe/config/AuditorAwareImpl.java
@@ -1,6 +1,9 @@
 package com.sysmatic2.finalbe.config;
 
+import com.sysmatic2.finalbe.member.dto.CustomUserDetails;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -12,17 +15,15 @@ public class AuditorAwareImpl implements AuditorAware<Long> {
     // 현재 사용자의 ID나 이름을 반환.
     // Optional을 사용하여 null이 될 수 있음을 처리
     public Optional<Long> getCurrentAuditor() {
-        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // 인증 정보가 없거나 인증이 되어 있지 않으면 빈 Optional을 반환
-//        if (authentication == null || !authentication.isAuthenticated()) {
-//            return Optional.empty();
-//        }
+//         인증 정보가 없거나 인증이 되어 있지 않으면 빈 Optional을 반환
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
 
-        // 인증된 사용자의 이름 반환 (username 또는 ID)
-//        return Optional.of(authentication.getName());
-
-        // 실제 구현에서 인증된 사용자 ID를 반환하도록 설정 (예: 1L은 임시 사용자 ID)
-        return Optional.of(1L);
+        // CustomUserDetails에서 memberId를 가져옴
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return Optional.ofNullable(userDetails.getMemberId());
     }
 }
---
+++ src/main/java/com/sysmatic2/finalbe/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sysmatic2.finalbe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth
+                .anyRequest().permitAll());
+
+        http.csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+}
