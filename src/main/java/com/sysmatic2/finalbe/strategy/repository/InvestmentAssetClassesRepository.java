546f8b05f4642c1928d761cda6d8abb6954694a5
kdh10806 <kimdaehyun10806@gmail.com>
2024-11-13 22:53:54 +0900

    [sync]

--- src/main/java/com/sysmatic2/finalbe/strategy/repository/InvestmentAssetClassesRepository.java
+++ src/main/java/com/sysmatic2/finalbe/strategy/repository/InvestmentAssetClassesRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,4 +22,7 @@ public interface InvestmentAssetClassesRepository extends JpaRepository<Investme
 
     //Pagination 적용
     Page<InvestmentAssetClassesEntity> findAll(Pageable pageable);
+
+    //전략 등록화면 투자자산목록 보내기 isActive = Y인 것만 출력, 순서 적용
+    List<InvestmentAssetClassesEntity> findByIsActiveOrderByOrderAsc(String isActive);
 }
