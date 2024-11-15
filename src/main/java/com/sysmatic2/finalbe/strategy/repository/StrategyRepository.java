49b875d91d6e1d3f61c879c16e7902417117dba3
Sang-Ho Jeon <67211899+Sang-Ho-Jeon@users.noreply.github.com>
2024-11-15 23:40:05 +0900

    add querydsl-dependency

--- src/main/java/com/sysmatic2/finalbe/strategy/repository/StrategyRepository.java
+++ src/main/java/com/sysmatic2/finalbe/strategy/repository/StrategyRepository.java
@@ -2,13 +2,14 @@ package com.sysmatic2.finalbe.strategy.repository;
 
 import com.sysmatic2.finalbe.strategy.entity.StrategyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
-public interface StrategyRepository extends JpaRepository<StrategyEntity, Long> {
+public interface StrategyRepository extends JpaRepository<StrategyEntity, Long>, QuerydslPredicateExecutor<StrategyEntity> {
     // 작성자 ID로 전략 목록 조회
     List<StrategyEntity> findByWriterId(Long writerId);
 
