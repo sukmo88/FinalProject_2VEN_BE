bfd1cd26f45664819e168046968e44de58d7ad66
superwalk <sukmo88@naver.com>
2024-11-21 22:12:33 +0900

    Fix: 로그인 api수정

---
+++ src/main/java/com/sysmatic2/finalbe/member/dto/LoginDto.java
@@ -0,0 +1,23 @@
+package com.sysmatic2.finalbe.member.dto;
+
+public class LoginDto {
+    private String id;
+    private String password;
+
+    // Getters & Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
