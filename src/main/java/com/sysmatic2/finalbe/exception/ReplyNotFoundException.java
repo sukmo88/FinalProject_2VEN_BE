e8518898c55c168c4517d64e9b59616a9a84f08a
haechangchoi <buraldongja@naver.com>
2024-11-22 17:21:10 +0900

    feat: 상담 모듈에 커스텀 예외 처리 로직 추가
    ### 추가된 내용
    - ConsultationNotFoundException: 상담 ID를 찾을 수 없을 때 발생
    - ReplyNotFoundException: 답변이 존재하지 않거나 완료되지 않았을 때 발생
    - ConsultationAlreadyCompletedException: 이미 완료된 상담에 대해 수정/삭제 시 발생
    - StrategyNotFoundException: 전략이 존재하지 않을 때 발생
    - InvestorNotFoundException: 투자자가 존재하지 않을 때 발생
    - TraderNotFoundException: 트레이더가 존재하지 않을 때 발생
    
    ### 수정된 내용
    - 서비스 레이어에서 기존 IllegalStateException 대신 커스텀 예외 사용
    - 테스트 코드에서 새로운 예외 상황에 대한 테스트 케이스 추가

---
+++ src/main/java/com/sysmatic2/finalbe/exception/ReplyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sysmatic2.finalbe.exception;
+
+public class ReplyNotFoundException extends RuntimeException {
+  public ReplyNotFoundException(String message) {
+    super(message);
+  }
+}
