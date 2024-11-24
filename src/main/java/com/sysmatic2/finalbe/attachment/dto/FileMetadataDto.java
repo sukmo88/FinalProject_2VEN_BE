1a55a79a2e6e6c6b422d7da19c7b300d994ea3df
ronnieOO <ibsb92@gmail.com>
2024-11-24 15:35:50 +0900

    feat(File) : add profile image uploaded to S3

--- src/main/java/com/sysmatic2/finalbe/attachment/dto/FileMetadataDto.java
+++ src/main/java/com/sysmatic2/finalbe/attachment/dto/FileMetadataDto.java
@@ -15,14 +15,15 @@ public class FileMetadataDto {
     private Long id;
     private String displayName; // 유저에게 보여질 파일 이름
     // private String fileName; // 저장된 파일 이름(UUID)
-    // private String filePath;
+    private String filePath;
     private Long fileSize;
     private String contentType;
-    private String fileCategory; // PROFILE, PROPOSAL, ICON
+    private String fileCategory; // profile, icon, proposal, account
+    private String fileCategoryItemId;
     private String uploaderId;
     // private boolean isDeleted;  // Soft Delete 여부)
     //private LocalDateTime deletedAt;
-    //private LocalDateTime uploadedAt;
+    private LocalDateTime uploadedAt;
     //private String description;
 
 
@@ -30,8 +31,11 @@ public class FileMetadataDto {
         this.id = fileMetadata.getId();
         this.displayName = fileMetadata.getDisplayName();
         this.fileSize = fileMetadata.getFileSize();
+        this.filePath = fileMetadata.getFilePath();
         this.contentType = fileMetadata.getContentType();
-        this.fileCategory = fileMetadata.getFileCategory();
         this.uploaderId = fileMetadata.getUploaderId();
+        this.fileCategory = fileMetadata.getFileCategory();
+        this.fileCategoryItemId = fileMetadata.getFileCategoryItemId();
+        this.uploadedAt = fileMetadata.getUploadedAt();
     }
 }
