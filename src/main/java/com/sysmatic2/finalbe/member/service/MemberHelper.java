40874dd7024ab1c9ba872c8d0a1b812bc1571fed
ronnieOO <ibsb92@gmail.com>
2024-11-29 03:48:16 +0900

    feat(Profile): modify Profile logic

---
+++ src/main/java/com/sysmatic2/finalbe/member/service/MemberHelper.java
@@ -0,0 +1,31 @@
+package com.sysmatic2.finalbe.member.service;
+
+import com.sysmatic2.finalbe.exception.MemberNotFoundException;
+import com.sysmatic2.finalbe.member.entity.MemberEntity;
+import com.sysmatic2.finalbe.member.repository.MemberRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberHelper {
+
+    private final MemberRepository memberRepository;
+
+    public MemberHelper(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void updateMemberFileId(String uploaderId, String fileId) {
+        MemberEntity memberEntity = memberRepository.findById(uploaderId)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found for uploaderId: " + uploaderId));
+        memberEntity.setFileId(fileId);
+        memberRepository.save(memberEntity);
+    }
+
+    public void initMemberFileId(String uploaderId, String fileId, String filePath) {
+        MemberEntity memberEntity = memberRepository.findById(uploaderId)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found for uploaderId: " + uploaderId));
+        memberEntity.setFileId(fileId);
+        memberEntity.setProfilePath(filePath);
+        memberRepository.save(memberEntity);
+    }
+}
